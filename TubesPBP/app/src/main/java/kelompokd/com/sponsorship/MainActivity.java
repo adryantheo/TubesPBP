2772ddf2100c38264e80b43a5a674fa62d801b12
adryantheo <43287907+adryantheo@users.noreply.github.com>
2018-09-18 02:05:36 +0700

    Progres Email Verifikasi

--- TubesPBP/app/src/main/java/kelompokd/com/sponsorship/MainActivity.java
+++ TubesPBP/app/src/main/java/kelompokd/com/sponsorship/MainActivity.java
@@ -1,13 +1,27 @@
 package kelompokd.com.sponsorship;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
     CheckBox role2;
     Button btnSingUp;
     Button btnLogin;
+    FirebaseAuth mAuth;
+    DatabaseReference mdatabase;
+    String Username, Email, Password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,9 +50,15 @@ public class MainActivity extends AppCompatActivity {
         btnSingUp=(Button) findViewById(R.id.btnSignUp);
         btnLogin=(Button) findViewById((R.id.btnLogin));
 
+        mdatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
+
         btnSingUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(v==btnSingUp){
+                    UserRegister();
+                }
                 Intent intent=new Intent(MainActivity.this,PendaftaranBerhasil.class);
                 startActivity(intent);
             }
@@ -64,4 +88,64 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void UserRegister(){
+        Username = username.getText().toString().trim();
+        Email = email.getText().toString().trim();
+        Password = password.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+                   sendEmailVerification();
+                   OnAuth(task.getResult().getUser());
+                   mAuth.signOut();
+               }else{
+                   Toast.makeText(MainActivity.this, "Gagal Daftar",Toast.LENGTH_SHORT).show();
+               }
+            }
+        });
+    }
+
+    private void sendEmailVerification(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user!=null){
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this,"Silahkan Cek Email Untuk Verifikasi",Toast.LENGTH_SHORT).show();
+                        FirebaseAuth.getInstance().signOut();
+                    }
+                }
+            });
+        }
+    }
+
+    private void OnAuth(FirebaseUser user){
+        createAnewUser(user.getUid());
+    }
+
+    private void createAnewUser(String uid){
+        User user = BuildNewUser();
+        mdatabase.child(uid).setValue(user);
+    }
+
+    private User BuildNewUser(){
+        return new User(
+                getDisplayName(),
+                getUserEmail(),
+                new Date().getTime()
+        );
+    }
+
+    public String getDisplayName(){
+        return Username;
+    }
+
+    public String getUserEmail(){
+        return Email;
+    }
+
 }
---
+++ TubesPBP/app/src/main/java/kelompokd/com/sponsorship/User.java
@@ -0,0 +1,29 @@
+package kelompokd.com.sponsorship;
+
+public class User {
+    String Displayname;
+
+
+    String Email;
+    long createdAt;
+
+    public User (){};
+    public User(String displayname,String email,long createdAt){
+        this.Displayname=displayname;
+        this.Email=email;
+        this.createdAt=createdAt;
+    }
+
+
+    public String getDisplayname() {
+        return Displayname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+}
